608ae21ea1d0f893c11bd1d08381738886109f5a
cemal can yıldırım <56302638+cemcois1@users.noreply.github.com>
2020-08-09 13:08:02 +0300

    init

---
+++ Main.py
@@ -0,0 +1,23 @@
+import yaml  #pip ile indirdim import ettim
+from tkinter import *# pip install PyYAML
+
+
+root=Tk()
+root.configure(bg="#585D3C")
+root.title("General Information About Countries")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+root.mainloop()
